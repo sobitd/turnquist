87d4a77d8fed395165827d14fcdc0ec3eb8b71f5
sobitd <sobitdaniel@gmail.com>
2025-06-18 06:03:40 +0300

    initial commit

---
+++ src/main/java/com/sobitd/project/turnquist/TurnquistApplication.java
@@ -0,0 +1,13 @@
+package com.sobitd.project.turnquist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnquistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnquistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobitd/project/turnquist/TurnquistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobitd.project.turnquist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnquistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
