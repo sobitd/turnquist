2af34db3a0f455622c4462ac6cc521e106b560f2
sobitd <sobitdaniel@gmail.com>
2025-06-22 22:26:26 +0300

    implement auth feature

--- src/main/java/com/sobitd/project/turnquist/config/SecurityConfig.java
+++ src/main/java/com/sobitd/project/turnquist/config/SecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import org.springframework.http.HttpMethod;
+
+
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -34,6 +40,23 @@ public class SecurityConfig {
             repository.save(new UserAccount("user", "password","ROLE_USER"));
             repository.save(new UserAccount("admin", "password","ROLE_ADMIN"));
         };
+    }
+
+    @Bean
+    SecurityFilterChain configureSecurity(HttpSecurity http)
+    throws Exception {
+        http.authorizeHttpRequests()
+            .requestMatchers("/login").permitAll()
+            .requestMatchers("/", "/search").authenticated()
+            .requestMatchers(HttpMethod.GET, "/api/**")
+            .authenticated()
+            .requestMatchers(HttpMethod.POST, "/new-video","/api/**").hasRole("ADMIN")
+            .anyRequest().denyAll()
+            .and()
+            .formLogin()
+            .and()
+            .httpBasic();
+        return http.build();
     }
 }
 
